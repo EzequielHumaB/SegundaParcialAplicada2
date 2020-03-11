4814b0efeb5193119fb2949791f1efa567fcddc8
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-11 16:49:32 -0400

    Creacion de la carpeta Data

---
+++ BlazorAppSegundoParcial/Data/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppSegundoParcial.Data
+{
+    public class Contexto
+    {
+    }
+}
