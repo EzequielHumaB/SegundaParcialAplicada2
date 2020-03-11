992f4e6786e23f77d725db4e4b4209750993f837
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-11 16:56:31 -0400

    Instalando Entity framework core y tools, ademas de crear la carpeta modelos, controladores y database

--- BlazorAppSegundoParcial/Data/Contexto.cs
+++ BlazorAppSegundoParcial/Data/Contexto.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorAppSegundoParcial.Data
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite(@"Data Source = Database/BaseDeDatosSegundoParcial"));
+        }
     }
 }
